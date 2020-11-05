b3e48173db23fcc5194e225ba5e8fd8b08e17aad
Crowdbotics <team@crowdbotics.com>
2020-11-05 20:06:11 +0000

    Initial commit.

---
+++ backend/hintted_22336/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hintted_22336 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hintted_22336.settings')
+
+application = get_wsgi_application()
